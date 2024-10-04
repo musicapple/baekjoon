3cabe5f06310a0c8e69a4826d6a716e89ffd8d38
musicapple <jaker0109@naver.com>
2024-10-05 01:06:16 +0900

    n진법 to 10진법 변환 문제

--- src/main/java/baekjoon2745/Main.java
+++ src/main/java/baekjoon2745/Main.java
@@ -15,12 +15,12 @@ public class Main {
 
         for(int i = B.length()-1; i>=0; i--){   // B 문자열의 맨 오른쪽부터 검사
             char c = B.charAt(i);   // B 문자열의 맨 오른쪽부터 문자타입으로 변환 ex) apple -> e
-            if(c>=0 && c<=9){       // c가 '0'~'9'의 문자인 경우
+            if(c>='0' && c<='9'){       // c가 '0'~'9'의 문자인 경우
                 num = c-'0';
             }else {                 // c가 'A'~'Z'인 경우
                 num = c - 55;
-                result += num*Math.pow(N,idx++);
             }
+            result += num*Math.pow(N,idx++);
         }
         System.out.println(result);
     }
