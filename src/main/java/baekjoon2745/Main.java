11881e7d663fcedb55adaeaf50bc716cefce41ad
musicapple <jaker0109@naver.com>
2024-10-05 01:33:33 +0900

    n진법 to 10진법 변환 문제

--- src/main/java/baekjoon2745/Main.java
+++ src/main/java/baekjoon2745/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String B = sc.nextLine();
+        String B = sc.next();
         int N = sc.nextInt();
         int num=0;
         int idx = 0;
@@ -16,9 +16,9 @@ public class Main {
         for(int i = B.length()-1; i>=0; i--){   // B 문자열의 맨 오른쪽부터 검사
             char c = B.charAt(i);   // B 문자열의 맨 오른쪽부터 문자타입으로 변환 ex) apple -> e
             if(c>='0' && c<='9'){       // c가 '0'~'9'의 문자인 경우
-                num = c-'0';
+                num = c-'0';        //   c를 10진수로 변환
             }else {                 // c가 'A'~'Z'인 경우
-                num = c - 55;
+                num = c - 55;       //   c를 10진수로 변환
             }
             result += num*Math.pow(N,idx++);
         }
