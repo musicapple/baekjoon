2fa5d4bc6f57e4e5095774776b0680687cdb95ff
sun <jaker0109@naver.com>
2024-10-11 09:14:57 -0700

    커밋

--- src/main/java/baekjoon2563/Main.java
+++ src/main/java/baekjoon2563/Main.java
@@ -1,7 +1,30 @@
 package baekjoon2563;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[][] paper = new int[100][100];
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = 0;
+
+        for(int i=0; i<num; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for(int x = a; x<a+10; x++){
+
+                for(int y = b; y<b+10; y++){
+                    if(paper[x][y]==0){
+                    paper[x][y] = 1;
+                    result += paper[x][y];
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
