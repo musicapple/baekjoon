262d697e924cfaae6ebb526e3f04bb01be6df295
sun <jaker0109@naver.com>
2024-10-11 09:50:10 -0700

    커밋

---
+++ src/main/java/baekjoon2720/Main.java
@@ -0,0 +1,22 @@
+package baekjoon2720;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i =0; i<t; i++) {
+            int c = sc.nextInt();
+            int q = c/25;
+            int d = (c%25)/10;
+            int n = ((c%25)%10)/5;
+            int p = (((c%25)%10)%5)/1;
+
+            System.out.println(q+" "+d+" "+n+" "+p);
+        }
+    }
+}
