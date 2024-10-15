3e3d812feb20ff83f65ebea268b7f1dc785930df
sun <jaker0109@naver.com>
2024-10-15 07:13:49 -0700

    커밋

--- src/main/java/baekjoon2501/Main.java
+++ src/main/java/baekjoon2501/Main.java
@@ -8,6 +8,22 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
+        int cnt = 0;
+
+        for(int i = 1; i<=n; i++){
+            if(n%i == 0){
+              cnt++;
+              if(cnt == k){
+                  System.out.println(i);
+                  break;
+              }
+            }
+        }
+        if(cnt<k){
+            System.out.println("0");
+        }
     }
 }
