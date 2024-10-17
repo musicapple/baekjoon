e41cecbe04d8b6debccd4f140ea04041c7a786f2
sun <jaker0109@naver.com>
2024-10-17 04:40:47 -0700

    커밋

---
+++ src/main/java/baekjoon10988/Main.java
@@ -0,0 +1,24 @@
+package baekjoon10988;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] c = str.toCharArray();
+        boolean flag = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (c[i] != c[str.length()-1 - i]) {
+                flag = false;
+                break;
+            } else flag = true;
+        }
+        if (flag) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
