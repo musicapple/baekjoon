0a8e8c8f885dc7ba73141937b4b9bb4f3c35e0b9
sun <jaker0109@naver.com>
2024-10-20 02:58:02 -0700

    커밋

--- src/main/java/baekjoon2941/Main.java
+++ src/main/java/baekjoon2941/Main.java
@@ -2,19 +2,20 @@ package baekjoon2941;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         String str = sc.next();
-        int len = str.length();
 
-        for (int i = 0; i < str.length(); i++) {
-            if (i < str.length()-1) {
-                if (str.charAt(i) == 'c') {
-                    if (str.charAt(i + 1) == '=' ||str.charAt(i+1) ==  '-') {
-                        len--;
+        int length = str.length();
+
+        for(int i=0; i<str.length(); i++){
+            if(i<str.length()-1){
+                if(str.charAt(i) == 'c'){
+                    if(str.charAt(i+1) == '=' || str.charAt(i+1)=='-'){
+                        length--;
                         i++;
                         continue;
                     }
@@ -23,40 +24,41 @@ public class Main {
             if(i<str.length()-2){
                 if(str.charAt(i) == 'd'){
                     if(str.charAt(i+1) == 'z'){
-                        if(str.charAt(i+2) == '='){
-                            len=len-2;
+                        if(str.charAt(i+2)== '='){
+                            length = length-2;
                             i+=2;
                             continue;
                         }
                     }
                 }
-            }if(i<str.length()-1){
+            }
+            if(i<str.length()-1){
                 if(str.charAt(i) == 'd'){
                     if(str.charAt(i+1) == '-'){
-                        len--;
+                        length--;
                         i++;
                         continue;
                     }
                 }
             }
             if(i<str.length()-1){
-                if(str.charAt(i) == 'l'||str.charAt(i) == 'n'){
+                if(str.charAt(i) == 'l' || str.charAt(i) == 'n'){
                     if(str.charAt(i+1) == 'j'){
-                        len--;
+                        length--;
                         i++;
                         continue;
                     }
                 }
             }
             if(i<str.length()-1){
-                if(str.charAt(i) == 's'||str.charAt(i) == 'z'){
+                if(str.charAt(i) == 's' || str.charAt(i) == 'z'){
                     if(str.charAt(i+1) == '='){
-                        len--;
+                        length--;
                         i++;
                     }
                 }
             }
         }
-        System.out.println(len);
+        System.out.println(length);
     }
 }
