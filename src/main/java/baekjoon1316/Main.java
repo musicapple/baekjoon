5577d3963e990ea927f3ac42c4eae99ca622283b
sun <jaker0109@naver.com>
2024-10-22 07:53:29 -0700

    커밋

--- src/main/java/baekjoon1316/Main.java
+++ src/main/java/baekjoon1316/Main.java
@@ -7,21 +7,24 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        boolean[] flag = new boolean[26];
+        int n = sc.nextInt();   // 그룹단어인지 체크할 단어 n개를 입력 받음
+        int group = n;
 
-
-        for(int i =0; i<n; i++){
+        for(int i = 0; i<n; i++){
             String str = sc.next();
-            char[] c = str.toCharArray();
+            char[] c = str.toCharArray();   // 단어가 중복되는 체크해야하니까 String을 char타입으로 쪼갬
+            boolean[] flag = new boolean[26];   // 26개의 알파벳이 중복/미중복인지 결과를 담음
 
-            
-            for(int j=0; j<str.length(); j++){
-                if(flag[c[j]-'a'] == false){
-
+            for(int j=0; j<str.length(); j++){  // 단어의 길이만큼 순회
+                if(flag[c[j] - 'a'] == false){  // 단어의 각 알파벳이 중복/미중복인지 확인
+                    flag[c[j]-'a'] = true;
+                }else if(j>0 && (flag[j]!=flag[j-1])){
+                        group--;
+                        break;
+                    // 그룹단어임 종료. 다음 단어 그룹 단어 체크
                 }
             }
         }
-
+        System.out.println(group);
     }
 }
