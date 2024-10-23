171d85fe4dabca991037338757d3ab06eb584937
sun <jaker0109@naver.com>
2024-10-23 07:30:50 -0700

    커밋

--- src/main/java/baekjoon2869/Main.java
+++ src/main/java/baekjoon2869/Main.java
@@ -1,8 +1,26 @@
 package baekjoon2869;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int v = sc.nextInt();
+        int tall=0;
+        int day = 0;
+        while(true){
+            day++;
+            tall += a;
+            if(tall >= v){
+                break;
+            }else{
+                tall -= b;
+            }
+        }
+        System.out.println(day);
     }
 }
