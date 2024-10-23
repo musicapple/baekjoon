9eb37f08fa1108901b0803b7fbe8124bc35d7d66
sun <jaker0109@naver.com>
2024-10-23 07:45:16 -0700

    커밋

--- src/main/java/baekjoon2869/Main.java
+++ src/main/java/baekjoon2869/Main.java
@@ -7,19 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int v = sc.nextInt();
-        int tall=0;
-        int day = 0;
-        while(true){
+        int a = sc.nextInt();   // 낮에 이동하는 거리
+        int b = sc.nextInt();   // 밤에 떨어지는 거리
+        int v = sc.nextInt();   // 최종 목표 거리
+
+        int distance= a-b;  // 하루동안 이동거리
+        int day = (v-b)/ distance; // 최종 목표까지 걸리는 일 수, 단, 나머지가 있을 경우 하루 추가
+
+        if((v-b)%distance!=0){
             day++;
-            tall += a;
-            if(tall >= v){
-                break;
-            }else{
-                tall -= b;
-            }
         }
         System.out.println(day);
     }
