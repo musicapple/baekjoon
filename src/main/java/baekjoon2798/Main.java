e7c3ba8f601db2736f9653558cc44361d29b1c36
sun <jaker0109@naver.com>
2024-10-28 09:09:35 -0700

    커밋

--- src/main/java/baekjoon2798/Main.java
+++ src/main/java/baekjoon2798/Main.java
@@ -8,40 +8,37 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        int[] arr = new int[n]; // 각 카드에 써져있는 숫자
-        for(int i=0; i<n; i++){
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) {
             arr[i] = sc.nextInt();
         }
-
-        int result = search(arr,n,m);
+        int result = cardGame(arr,N,M);
         System.out.println(result);
+
     }
 
-    static int search(int[] arr, int n, int m){
-        int result = 0;
-        int sum = 0;
+    public static int cardGame(int[] arr, int N, int M){
+        int sum;
+        int save = 0;
+        for(int i = 0; i<N-2; i++) {
+            for(int j= i+1; j<N-1; j++) {
+                for(int k = j+1; k<N; k++) {
 
-        for(int i = 0; i<n-2; i++) {    // i는 0번째 자리부터 n-2번째 자리까지 순회
-
-            for(int j = i+1; j<n-1; j++) {  //  j는 i+1자리부터 n-1번째 자리까지 순회
-
-                for(int k= j+1; k<n; k++){  // k는 j+1자리부터 n번째 자리까지 순회
                     sum = arr[i] + arr[j] + arr[k];
-
-                    if(sum == m){
+                    if(sum == M) {
                         return sum;
                     }
-                    if(sum < m){  // 현재합이 m보다 작은 경우
-                        if(result < sum) {  // 이전의 합이 현재합보다 작은 경우에만 코드 실행
-                            result = sum;
+                    if(sum<M) {
+                        if(save<sum) {
+                            save = sum;
                         }
                     }
                 }
             }
         }
-        return result;
+        return save;
     }
 }
